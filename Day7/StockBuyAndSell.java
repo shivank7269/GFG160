26231f39c38cc0fcfa29c2662ae35d0a20f2b42f
Coderx8972 <s89722020@gmail.com>
2025-03-26 20:59:09 +0530

    Solved in O(n) time complexity

--- Day7/StockBuyAndSell.java
+++ Day7/StockBuyAndSell.java
@@ -1,4 +1,18 @@
 package Day7;
 
 public class StockBuyAndSell {
+    public int maximumProfit(int prices[]) {
+        int maxprofit =0;
+        int n=prices.length;
+        int buy=0,sell=0;
+        for(int i=1;i<n;i++){
+            if(prices[i-1]>prices[i]){
+                maxprofit+=prices[i-1]-prices[buy];
+                buy=i;
+            }
+        }
+        sell=n-1;
+        maxprofit+=prices[sell]-prices[buy];
+        return maxprofit;
+    }
 }

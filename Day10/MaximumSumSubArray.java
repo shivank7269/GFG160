43117aeedcf37012f990cfa2fbb3aafb7e014d8c
Coderx8972 <s89722020@gmail.com>
2025-03-29 13:54:49 +0530

    Solved the Maximum Sum Sub Array by Kadenes Algorithm

--- Day10/MaximumSumSubArray.java
+++ Day10/MaximumSumSubArray.java
@@ -1,7 +1,7 @@
 package Day10;
 
 public class MaximumSumSubArray {
-    int maxSubarraySum(int[] arr) {
+    static int maxSubarraySum(int[] arr) {
         int n=arr.length;
         int sum=0;
         int res=Integer.MIN_VALUE;
@@ -11,5 +11,11 @@ public class MaximumSumSubArray {
             if(sum<0) sum=0;
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int ans = maxSubarraySum(arr);
+        System.out.println("Answer :"+ans);
     }
 }
